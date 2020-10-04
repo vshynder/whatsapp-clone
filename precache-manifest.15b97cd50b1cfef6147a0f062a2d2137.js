be0a85ba1c59766edefcf3e2c9edad6e5e6dfcc9
Vlad Shynder <shynderv@gmail.com>
2020-10-04 21:25:14 +0300

    Updates

---
+++ precache-manifest.15b97cd50b1cfef6147a0f062a2d2137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1c3f74516a5e52af588ec17d2b94a5",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "fc2c2f69eb711f6702d1",
+    "url": "/whatsapp-clone/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fc2c2f69eb711f6702d1",
+    "url": "/whatsapp-clone/static/js/2.59c858d3.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/whatsapp-clone/static/js/2.59c858d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42207fe28ae1d985b27",
+    "url": "/whatsapp-clone/static/js/main.1606399a.chunk.js"
+  },
+  {
+    "revision": "e65330cf6b1290ce3e1c",
+    "url": "/whatsapp-clone/static/js/runtime-main.ca335755.js"
+  }
+]);
